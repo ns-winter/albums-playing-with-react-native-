49976cc8ad4520de17804c5505a0b90550582672
Nigel Said <nigelsaid@gmail.com>
2017-06-06 14:42:15 +0100

    adds Header component

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,13 @@
 //IOS code in here...
 
+//Root Component
+
 //Import Library to help create Component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
 
 //Create a Component
-//can be refactored as so:
-// const App = () => (
-//   <Text>Hello, World!</Text>
-// );
+
 
 const App = () => {
   return (
@@ -16,7 +15,11 @@ const App = () => {
   );
 };
 
+//can be refactored as so:
+// const App = () => (
+//   <Text>Hello, World!</Text>
+// );
 
 
-//render it to the device
+//render it to the device (since it is the Root/Parent Component)
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+//Child component
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+//make component available to other parts of the app
+export default Header;
