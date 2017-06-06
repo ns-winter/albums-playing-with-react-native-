29b3ce1c39e4a1b118d0954caf152222a328fe41
Nigel Said <nigelsaid@gmail.com>
2017-06-06 20:08:57 +0100

    adds Album Image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,12 +4,13 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({album}) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const {
     thumbnailStyle,
     thumbnailContainerStyle,
     headerContentStyle,
-    headerTextStyle
+    headerTextStyle,
+    imageStyle
   } = styles;
 
   return (
@@ -25,6 +26,11 @@ const AlbumDetail = ({album}) => {
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }} />
       </CardSection>
     </Card>
   );
@@ -50,6 +56,12 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
